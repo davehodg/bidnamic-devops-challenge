df42ce659e6c6d6c8a09649c7c5442c835c24e67
David Hodgkinson <daveh@hodgkinson.org>
2022-01-20 10:09:43 +0000

    got tf working

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ terraform {
 
 provider "aws" {
       profile = "default"  
-      region  = "us-west-2"
+      region  = "eu-west-2"
       }
 
 resource "aws_instance" "app_server" {
-    ami           = "ami-830c94e3"  
+    ami           = "ami-0fdbd8587b1cf431e"  
     instance_type = "t2.micro"
   
     tags = {    
